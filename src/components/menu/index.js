d4293b09630be0c635989f843fb7c2af2074e8ee
LeXik0N <lexik0n1993@yandex.ru>
2021-04-24 18:14:17 +0600

    fixed path to deploy and added some localization

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,4 +1,10 @@
 const menuItems = ['dashboard', 'products', 'categories', 'sales'];
+const menuLocales = {
+  dashboard: 'Панель управления',
+  products: 'Товары',
+  categories: 'Категории',
+  sales: 'Продажи'
+};
 
 const URL_PATH = process.env.URL_PATH;
 
@@ -11,7 +17,7 @@ export default function renderMenu(path) {
         <li ${match ? 'class="active"' : ''}>
           <a href="/${URL_PATH}${item === 'dashboard' ? '' : item}" data-page="${item}">
             <i class="icon-${item}"></i>
-            <span>${item}</span>
+            <span>${menuLocales[item]}</span>
           </a>
         </li>
       `;
--- src/index.js
+++ src/index.js
@@ -27,29 +27,29 @@ class MainPage {
           <ul class="sidebar__nav">
             <li>
               <a href="/${URL_PATH}" data-page="dashboard">
-                <i class="icon-dashboard"></i> <span>Dashboard</span>
+                <i class="icon-dashboard"></i> <span>Панель управления</span>
               </a>
             </li>
             <li>
               <a href="/${URL_PATH}products" data-page="products">
-                <i class="icon-products"></i> <span>Products</span>
+                <i class="icon-products"></i> <span>Товары</span>
               </a>
             </li>
             <li>
               <a href="/${URL_PATH}categories" data-page="categories">
-                <i class="icon-categories"></i> <span>Categories</span>
+                <i class="icon-categories"></i> <span>Категории</span>
               </a>
             </li>
             <li>
               <a href="/${URL_PATH}sales" data-page="sales">
-                <i class="icon-sales"></i> <span>Sales</span>
+                <i class="icon-sales"></i> <span>Продажи</span>
               </a>
             </li>
           </ul>
           <ul class="sidebar__nav sidebar__nav_bottom">
             <li>
               <button type="button" class="sidebar__toggler">
-                <i class="icon-toggle-sidebar"></i> <span>Toggle sidebar</span>
+                <i class="icon-toggle-sidebar"></i> <span>Скрыть панель</span>
               </button>
             </li>
           </ul>
