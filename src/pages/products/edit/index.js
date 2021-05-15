59986a4510106d566aa12dcf817877ea6b7f9db4
Yuri <34709153+y-dubovitsky@users.noreply.github.com>
2021-05-15 11:19:01 +0300

    Added Products page.

--- src/pages/products/edit/index.js
+++ src/pages/products/edit/index.js
@@ -1,3 +1,5 @@
+import ProductForm from "../../../components/product-form-custom";
+
 export default class Page {
   element;
   subElements = {};
@@ -8,11 +10,33 @@ export default class Page {
 
     element.innerHTML = `
       <div>
-        <h1>Edit page</h1>
+        <h1>List page</h1>
       </div>`;
 
     this.element = element.firstElementChild;
 
+    this.initComponents();
+    await this.renderComponents();
+
     return this.element;
+  }
+
+  initComponents() {
+    const productId = '101-planset-lenovo-tab-p10-tb-x705l-32-gb-3g-lte-belyj';
+
+    this.components.productFrom = new ProductForm(productId);
+  }
+
+  async renderComponents() {
+    await this.components.productFrom.render();
+    const element = await this.components.productFrom.element;
+    console.log(element);
+    this.element.append(element);
+  }
+
+  destroy() {
+    for (const component of Object.values(this.components)) {
+      component.destroy();
+    }
   }
 }
--- src/pages/products/list/index.js
+++ src/pages/products/list/index.js
@@ -1,18 +1,46 @@
-import ProductForm from "../../../components/product-form";
+import SortableTable from "../../../components/sortable-table";
+import header from "./products-header.js";
 
 export default class Page {
   element;
   subElements = {};
   components = {};
 
+  getTemplate() {
+    return `
+      <div class="products-list">
+        <div class="content__top-panel">
+          <h1 class="page-title">Products</h1>
+          <a href="/products/add" class="button-primary">Add product</a>
+        </div>
+        <div class="content-box content-box_small">
+          <form class="form-inline">
+            <div class="form-group">
+              <label class="form-label">Filter:</label>
+              <input type="text" data-element="filterName" class="form-control" placeholder="Product name">
+            </div>
+            <div class="form-group" data-element="slider">
+              <label class="form-label">Price:</label>
+            </div>
+            <div class="form-group">
+              <label class="form-label">Status:</label>
+              <select class="form-control" data-element="filterStatus">
+                <option value="" selected="">Any</option>
+                <option value="1">Active</option>
+                <option value="0">Inactive</option>
+              </select>
+            </div>
+          </form>
+        </div>
+        <div class="products-list__container" data-element="sortableTable">
+        </div>
+      </div>
+    `;
+  }
+
   async render() {
     const element = document.createElement('div');
-
-    element.innerHTML = `
-      <div>
-        <h1>List page</h1>
-      </div>`;
-
+    element.innerHTML = this.getTemplate();
     this.element = element.firstElementChild;
 
     this.initComponents();
@@ -22,15 +50,26 @@ export default class Page {
   }
 
   initComponents() {
-    const productId = '101-planset-lenovo-yt3-x90l-64-gb-3g-lte-cernyj';
+    const priceMin = 0;
+    const priceMax = 4000;
 
-    this.components.productFrom = new ProductForm(productId);
+    this.components.sortableTable = new SortableTable(
+      header,
+      {
+        url: this.getProductUrl(priceMin, priceMax),
+      }
+    );
   }
 
   async renderComponents() {
-    const element = await this.components.productFrom.render();
-
+    const element = await this.components.sortableTable.element;
     this.element.append(element);
+  }
+
+  getProductUrl(priceMin, priceMax, filterName, status) {
+    return `api/rest/products?_embed=subcategory.category&price_gte=${priceMin}&price_lte=${priceMax}`
+      + (filterName ? `&title_like=${encodeURIComponent(filterName)}` : '')
+      + (status ? `&status=${status}` : '');
   }
 
   destroy() {
