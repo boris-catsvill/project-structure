a5ac2f47f88ef2706165df6089320965b0f9f2ea
Andrey Plotnikov <xret90@gmail.com>
2021-06-08 14:54:43 +0400

    implemented page ProductsList

--- src/pages/products/list/index.js
+++ src/pages/products/list/index.js
@@ -1,39 +1,87 @@
-import ProductForm from "../../../components/product-form";
+import SortableTable from "../../../components/sortable-table";
+import header from "./products-header.js";
 
 export default class Page {
-  element;
   subElements = {};
   components = {};
 
-  async render() {
+  render() {
     const element = document.createElement('div');
-
-    element.innerHTML = `
-      <div>
-        <h1>List page</h1>
-      </div>`;
+    element.innerHTML = this.getTemplate();
 
     this.element = element.firstElementChild;
+    this.subElements = this.getSubElements(this.element);
 
-    this.initComponents();
-    await this.renderComponents();
+
+    const components = this.initComponents();
+    this.renderComponents(components);
+    this.components = components;
 
     return this.element;
   }
 
   initComponents() {
-    const productId = '101-planset-lenovo-yt3-x90l-64-gb-3g-lte-cernyj';
+    this.urlProducts =  new URL('/api/rest/products', process.env.BACKEND_URL);
+    this.urlProducts.searchParams.set('_embed', 'subcategory.category');
 
-    this.components.productFrom = new ProductForm(productId);
+    const sortableTable = new SortableTable(header, {
+      url: this.urlProducts,
+      isSortLocally: false,
+      isInfinityScroll: true,
+      sorted: {
+        id: 'title',
+        order: 'asc'
+      }
+    });
+
+    return {
+      sortableTable
+    };
   }
 
-  async renderComponents() {
-    const element = await this.components.productFrom.render();
+  renderComponents(components) {
+    const keysComponents = Object.keys(components);
 
-    this.element.append(element);
+    keysComponents.forEach(component => {
+      const root = this.subElements[component];
+      const { element } = components[component];
+
+      root.append(element);
+    });
+  }
+
+  getSubElements(element) {
+    const result = {};
+    const elements = element.querySelectorAll('[data-element]');
+
+    for (const subElement of elements) {
+      const name = subElement.dataset.element;
+      result[name] = subElement;
+    }
+
+    return result;
+  }
+
+  getTemplate() {
+    return `
+      <div class="products-list">
+        <div class="content__top-panel">
+          <h1 class="page-title">Товары</h1>
+          <a href="/products/add" class="button-primary">Добавить товар</a>
+        </div>
+        <div class="content-box content-box_small"></div>
+        <div class="products-list__container" data-element="sortableTable"></div>
+      </div>
+    `;
+  }
+
+  remove() {
+    this.element.remove();
   }
 
   destroy() {
+    this.remove();
+
     for (const component of Object.values(this.components)) {
       component.destroy();
     }
