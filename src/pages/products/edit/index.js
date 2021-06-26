301ca66a0baaca34f5aba2a60042bb4f0ac499e5
AKurbatov <AKurbatov83@mail.ru>
2021-06-26 18:36:57 +1100

    Урок 11. Сборка проекта с помощью Webpack

--- src/pages/products/edit/index.js
+++ src/pages/products/edit/index.js
@@ -1,18 +1,84 @@
+import ProductForm from '../../../components/product-form/index.js';
+import NotificationMessage from '../../../components/notification/index.js';
+
 export default class Page {
-  element;
-  subElements = {};
-  components = {};
+    element;
+    subElements = {};
+    components = {};
 
-  async render() {
-    const element = document.createElement('div');
+    constructor(match) {
+        if (match)
+            this.productId = match[match.length - 1] != "products/add" ? match[match.length - 1] : undefined;
+    }
 
-    element.innerHTML = `
-      <div>
-        <h1>Edit page</h1>
-      </div>`;
+    get template() {
+        return `
+            <div class="products-edit">
+                <div class="content__top-panel">
+                    <h1 class="page-title">
+                        <a href="/products" class="link">Товары</a> / Редактировать
+                    </h1>
+                </div>
+                <div data-element="formEdit" class="content-box">
+                    
+                </div>
+            </div>
+        `;
+    }
+    
+    initComponents() {
+        const productForm = new ProductForm(this.productId);
+        this.components.productForm = productForm;
+    }
 
-    this.element = element.firstElementChild;
+    async renderComponents() {
+        const {productForm} = this.components;
 
-    return this.element;
-  }
+        await productForm.render();
+        this.subElements.formEdit.append(productForm.element);
+    }
+
+    async render() {
+        const element = document.createElement('div');
+
+        element.innerHTML = this.template;
+
+        this.element = element.firstElementChild;
+        this.subElements = this.getSubElements(this.element);
+
+        this.initComponents();
+        await this.renderComponents();
+        this.initEventListeners();
+        
+        return this.element;
+    }
+
+    initEventListeners () {
+        this.components.productForm.element.addEventListener("product-saved", event => {
+            const notification = new NotificationMessage("Товар добавлен");
+            notification.show();
+        });
+      
+        this.components.productForm.element.addEventListener("product-updated", event => {
+            const notification = new NotificationMessage("Товар сохранен");
+            notification.show();
+        })
+          
+    }
+        
+    getSubElements (element) {
+        const elements = element.querySelectorAll('[data-element]');
+    
+        return [...elements].reduce((accum, subElement) => {
+            accum[subElement.dataset.element] = subElement;
+    
+            return accum;
+        }, {});
+    }
+    
+    destroy() {
+        for (const component of Object.values(this.components)) {
+            component.destroy();
+        }
+    }
 }
--- src/pages/products/list/index.js
+++ src/pages/products/list/index.js
@@ -1,41 +1,163 @@
-import ProductForm from "../../../components/product-form";
+import SortableTable from '../../../components/sortable-table/index.js';
+import DoubleSlider from '../../../components/double-slider/index.js';
+import header from './products-header.js';
+
+class LinkSortableTable extends SortableTable {
+    getTableRows(data) {
+        return data.map(item => `
+            <a href="${`/products/${item.id}`}" class="sortable-table__row">
+                ${this.getTableRow(item, data)}
+            </a>`
+        ).join('');
+    }
+}
 
 export default class Page {
-  element;
-  subElements = {};
-  components = {};
+    element;
+    subElements = {};
+    components = {};
+    searchParams = {};
 
-  async render() {
-    const element = document.createElement('div');
-
-    element.innerHTML = `
-      <div>
-        <h1>List page</h1>
-      </div>`;
-
-    this.element = element.firstElementChild;
-
-    this.initComponents();
-    await this.renderComponents();
-
-    return this.element;
-  }
-
-  initComponents() {
-    const productId = '101-planset-lenovo-yt3-x90l-64-gb-3g-lte-cernyj';
-
-    this.components.productFrom = new ProductForm(productId);
-  }
-
-  async renderComponents() {
-    const element = await this.components.productFrom.render();
-
-    this.element.append(element);
-  }
-
-  destroy() {
-    for (const component of Object.values(this.components)) {
-      component.destroy();
+    get template() {
+        return `
+            <div class="products-list">
+                <div class="content__top-panel">
+                    <h1 class="page-title">Товары</h1>
+                    <a href="/products/add" class="button-primary">Добавить товар</a>
+                </div>
+                <div class="content-box content-box_small">
+                    <form class="form-inline">
+                        <div class="form-group">
+                            <label class="form-label">Сортировать по:</label>
+                            <input type="text" data-element="filterName" class="form-control" placeholder="Название товара">
+                        </div>
+                        <div class="form-group" data-elem="sliderContainer">
+                            <label class="form-label">Цена:</label>
+                            <!-- DoubleSlider component -->
+                            <div data-element="doubleSlider"></div>
+                        </div>
+                        <div class="form-group">
+                            <label class="form-label">Статус:</label>
+                            <select class="form-control" data-element="filterStatus">
+                                <option value="" selected="">Любой</option>
+                                <option value="1">Активный</option>
+                                <option value="0">Неактивный</option>
+                            </select>
+                        </div>
+                    </form>            
+                </div>
+                <div data-elem="productsContainer" class="products-list__container">
+                    <div data-element="sortableTable">
+                        <!-- sortable-table component -->
+                    </div>
+                </div>
+            </div>
+        `;
     }
-  }
+
+    async render() {
+        const element = document.createElement("div");
+
+        element.innerHTML = this.template;
+
+        this.element = element.firstElementChild;
+        this.subElements = this.getSubElements(this.element);
+
+        this.initComponents();
+        this.initSortableTable();
+
+        await this.renderComponents();
+        this.initEventListeners();
+
+        return this.element;
+    }
+
+    initSortableTable({
+        priceFrom,
+        priceTo,
+        titleLike,
+        status
+    } = {}) {
+        const url = new URL("api/rest/products", process.env.BACKEND_URL);
+        url.searchParams.append("_embed", "subcategory.category");
+        url.searchParams.append("_sort", "title");
+        url.searchParams.append("_order", "asc");
+        url.searchParams.append("_start", "0");
+        url.searchParams.append("_end", "30");
+        if (priceFrom != undefined) {
+            url.searchParams.append("price_gte", priceFrom);
+        }
+        if (priceTo != undefined) {
+            url.searchParams.append("price_lte", priceTo);
+        }
+        if (titleLike) {
+            url.searchParams.append("title_like", titleLike);
+        }
+        if (status) {
+            url.searchParams.append("status", status);
+        }
+        const sortableTable = new LinkSortableTable(header, {
+            url: url.href
+        });
+        this.components.sortableTable = sortableTable;
+    }
+
+    initComponents() {
+        const doubleSlider = new DoubleSlider({
+            min: 0,
+            max: 4000,
+            formatValue: value => '$' + value
+        });
+        this.components.doubleSlider = doubleSlider;
+    }
+
+    async renderComponents() {
+        const {doubleSlider, sortableTable} = this.components;
+
+        this.subElements.doubleSlider.append(doubleSlider.element);
+        this.subElements.sortableTable.append(sortableTable.element);
+    }
+
+    async updateTableComponent ({
+        priceFrom,
+        priceTo,
+        titleLike,
+        status
+    } = {}) {
+        this.initSortableTable(arguments[0]);
+        this.subElements.sortableTable.firstElementChild.replaceWith(this.components.sortableTable.element);
+    }
+
+    initEventListeners () {
+        this.subElements.filterName.addEventListener("input", event => {
+            this.searchParams.titleLike = event.target.value;
+            this.updateTableComponent(this.searchParams);
+        });
+        this.components.doubleSlider.element.addEventListener("range-select", event => {
+            const { from, to } = event.detail;
+            this.searchParams.priceFrom = from;
+            this.searchParams.priceTo = to;
+            this.updateTableComponent(this.searchParams);
+        });
+        this.subElements.filterStatus.addEventListener("change", event => {
+            this.searchParams.status = event.target.value;
+            this.updateTableComponent(this.searchParams);
+        });
+    }
+        
+    getSubElements (element) {
+        const elements = element.querySelectorAll('[data-element]');
+    
+        return [...elements].reduce((accum, subElement) => {
+          accum[subElement.dataset.element] = subElement;
+    
+          return accum;
+        }, {});
+    }
+    
+    destroy() {
+        for (const component of Object.values(this.components)) {
+            component.destroy();
+        }
+    }
 }
