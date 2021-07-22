513c6f5cbd3157c5ff5c7747294d208cec11de49
livanchikova <l.ivanchikova.c@gmail.com>
2021-07-22 22:28:12 +0300

    v2

--- src/components/column-chart/index.js
+++ src/components/column-chart/index.js
@@ -1,123 +1,89 @@
-import fetchJson from '../../utils/fetch-json.js';
-
-const BACKEND_URL = 'https://course-js.javascript.ru';
-
 export default class ColumnChart {
   element;
+  subElements = {};
   chartHeight = 50;
 
   constructor({
-                url = "",
-                range = {
-                  from: new Date(),
-                  to: new Date(),
-                },
-                label = "",
-                link = "",
-                formatHeading = (data) => data,
+                data = [],
+                label = '',
+                link = '',
+                value = 0
               } = {}) {
-    this.url = url;
-    this.range = range;
+    this.data = data;
     this.label = label;
     this.link = link;
-    this.formatHeading = formatHeading;
-    this.data = [];
-    this.value = 0;
+    this.value = value;
 
     this.render();
   }
 
-  async getData(from, to) {
-    const url = `${BACKEND_URL}/${this.url}`;
-    this.data = await fetchJson(
-      `${url}?from=${from.toISOString()}&to=${to.toISOString()}`
-    );
-    this.value = this.getValues(this.data);
+  getColumnBody(data) {
+    const maxValue = Math.max(...data);
 
-    this.subElements.body.innerHTML = this.getBody(this.data);
-    this.subElements.header.innerHTML = this.getHeader(this.value);
-    this.element.classList.remove("column-chart_loading");
+    return data
+      .map(item => {
+        const scale = this.chartHeight / maxValue;
+        const percent = (item / maxValue * 100).toFixed(0);
+
+        return `<div style="--value: ${Math.floor(item * scale)}" data-tooltip="${percent}%"></div>`;
+      })
+      .join('');
   }
 
-  getValues(data) {
-    return Object.values(data).reduce((a, b) => a + b, 0);
+  getLink() {
+    return this.link ? `<a class="column-chart__link" href="${this.link}">View all</a>` : '';
   }
 
-  render() {
-    const element = document.createElement("div");
-    const link = this.link
-      ? `<a href="${this.link}" class="column-chart__link">View all</a>`
-      : "";
-
-    element.innerHTML = `
-      <div class="column-chart column-chart_loading" style="--chart-height: ${
-      this.chartHeight
-    }">
+  get template () {
+    return `
+      <div class="column-chart column-chart_loading" style="--chart-height: ${this.chartHeight}">
         <div class="column-chart__title">
-          Total ${this.label}
-          ${link}
+          ${this.label}
+          ${this.getLink()}
         </div>
         <div class="column-chart__container">
           <div data-element="header" class="column-chart__header">
-            ${this.getHeader(this.value)}
+            ${this.value}
           </div>
           <div data-element="body" class="column-chart__chart">
-            ${this.getBody(this.data)}
+            ${this.getColumnBody(this.data)}
           </div>
         </div>
       </div>
     `;
+  }
 
+  async render() {
+    const element = document.createElement('div');
+
+    element.innerHTML = this.template;
     this.element = element.firstElementChild;
+
+    if (this.data.length) {
+      this.element.classList.remove(`column-chart_loading`);
+    }
+
     this.subElements = this.getSubElements(this.element);
 
-    this.update();
+    return this.element;
   }
 
-  getHeader(value) {
-    return this.formatHeading(value);
-  }
-
-  getBody(data) {
-    const dataValues = Object.values(data);
-    const maxValue = Math.max(...dataValues);
-    const scale = this.chartHeight / maxValue;
-
-    return dataValues
-      .map((item) => {
-        const percent = ((item / maxValue) * 100).toFixed(0) + "%";
-        const value = String(Math.floor(item * scale));
-        return `<div style="--value: ${value}" data-tooltip="${percent}"></div>`;
-      })
-      .join("");
-  }
-
-  getSubElements(element) {
-    const result = {};
+  getSubElements (element) {
     const elements = element.querySelectorAll('[data-element]');
 
-    for (let subElement of elements) {
-      const name = subElement.dataset.element;
-      result[name] = subElement;
-    }
+    return [...elements].reduce((accum, subElement) => {
+      accum[subElement.dataset.element] = subElement;
 
-    return result;
+      return accum;
+    }, {});
   }
 
-  async update(from = this.range.from, to = this.range.to) {
-    await this.getData(from, to);
-    return this.data;
-  }
-
-  remove() {
-    if(this.element){
-      this.element.remove();
-    }
+  update ({headerData, bodyData}) {
+    this.subElements.header.textContent = headerData;
+    this.subElements.body.innerHTML = this.getColumnBody(bodyData);
   }
 
   destroy() {
-    this.remove();
-    this.element = null;
-    this.subElements = {};
+    this.element.remove();
   }
 }
--- src/pages/dashboard/bestsellers-header.js
+++ src/pages/dashboard/bestsellers-header.js
@@ -1,7 +1,7 @@
 const header = [
   {
     id: 'images',
-    title: 'Image',
+    title: 'Фото',
     sortable: false,
     template: data => {
       return `
@@ -13,25 +13,25 @@ const header = [
   },
   {
     id: 'title',
-    title: 'Name',
+    title: 'Название',
     sortable: true,
     sortType: 'string'
   },
   {
     id: 'quantity',
-    title: 'Quantity',
+    title: 'Категория',
     sortable: true,
     sortType: 'number'
   },
   {
     id: 'price',
-    title: 'Price',
+    title: 'Цена',
     sortable: true,
     sortType: 'number'
   },
   {
     id: 'status',
-    title: 'Status',
+    title: 'Статус',
     sortable: true,
     sortType: 'number',
     template: data => {
