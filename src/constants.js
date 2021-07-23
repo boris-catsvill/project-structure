ae392bfc0924ce85f09d8cea49af1a9fec60b95e
Alexander Kryuk <akryuk@beetsoft.com>
2021-07-23 12:06:02 +0600

    - Product list page rendering

--- src/constants.js
+++ src/constants.js
@@ -1,40 +1,98 @@
 export const salesTableHeader = [
   {
-    id: "id",
-    title: "ID",
+    id: 'id',
+    title: 'ID',
     sortable: true,
-    sortType: "number"
+    sortType: 'number'
   },
   {
-    id: "user",
-    title: "Клиент",
+    id: 'user',
+    title: 'Клиент',
     sortable: true,
-    sortType: "string"
+    sortType: 'string'
   },
   {
-    id: "createdAt",
-    title: "Дата",
+    id: 'createdAt',
+    title: 'Дата',
     sortable: true,
-    sortType: "number",
+    sortType: 'number',
     template: data => {
       return `
         <div class="sortable-table__cell">
-            ${new Date(data).toLocaleString("default", {dateStyle: "medium"})}
+            ${new Date(data).toLocaleString('default', { dateStyle: 'medium' })}
         </div>
       `;
     }
   },
   {
-    id: "totalCost",
-    title: "Стоимость",
+    id: 'totalCost',
+    title: 'Стоимость',
     sortable: true,
-    sortType: "number",
+    sortType: 'number',
     template: data => `<div class="sortable-table__cell">$${data}</div>`
   },
   {
-    id: "delivery",
-    title: "Статус",
+    id: 'delivery',
+    title: 'Статус',
     sortable: true,
-    sortType: "string"
+    sortType: 'string'
+  }
+];
+
+export const productsTableHeader = [
+  {
+    id: 'images',
+    title: 'Фото',
+    sortable: false,
+    template: data => {
+      return `
+        <div class="sortable-table__cell">
+          ${data.length ? `<img class="sortable-table-image" alt="Image" src="${data[0].url}">` : ""}
+        </div>
+      `;
+    }
+  },
+  {
+    id: 'title',
+    title: 'Название',
+    sortable: true,
+    sortType: 'string'
+  },
+  {
+    id: 'subcategory',
+    title: 'Категория',
+    sortable: false,
+    template: subcategory => {
+      return `
+        <div class="sortable-table__cell">
+          ${subcategory.title}
+        </div>
+      `;
+    }
+  },
+  {
+    id: 'quantity',
+    title: 'Количество',
+    sortable: true,
+    sortType: 'number'
+  },
+  {
+    id: 'price',
+    title: 'Цена',
+    sortable: true,
+    sortType: 'number'
+  },
+  {
+    id: 'status',
+    title: 'Статус',
+    sortable: true,
+    sortType: 'number',
+    template: data => {
+      return `
+        <div class="sortable-table__cell">
+            ${data ? "Активен" : "Неактивен"}
+        </div>
+      `;
+    }
   },
 ];
--- src/pages/products/list/index.js
+++ src/pages/products/list/index.js
@@ -1,19 +1,47 @@
-import ProductForm from "../../../components/product-form";
+import SortableTable from '../../../components/sortable-table';
+import { productsTableHeader } from '../../../constants';
+import DoubleSlider from '../../../components/double-slider';
 
 export default class Page {
   element;
   subElements = {};
   components = {};
 
+  constructor() {
+    this.initComponents();
+  }
+
   async render() {
     const element = document.createElement('div');
 
     element.innerHTML = `
-      <div>
-        <h1>List page</h1>
+      <div class='products-list'>
+        <div class="content__top-panel">
+          <h1 class="page-title">Products</h1>
+          <a href="/products/add" class="button-primary">Add Product</a>
+        </div>
+        <div class="content-box content-box_small">
+          <form class="form-inline">
+          <div class="form-group">
+            <label class="form-label">Сортировать по:</label>
+            <input type="text" data-elem="filterName" class="form-control" placeholder="Название товара">
+          </div>
+          <div class="form-group" data-element="doubleSlider"></div>
+          <div class="form-group">
+            <label class="form-label">Статус:</label>
+            <select class="form-control" data-elem="filterStatus">
+              <option value="" selected="">Любой</option>
+              <option value="1">Активный</option>
+              <option value="0">Неактивный</option>
+            </select>
+          </div>
+        </form>
+        </div>
+        <div data-element='sortableTable'></div>
       </div>`;
 
     this.element = element.firstElementChild;
+    this.subElements = this.getSubElements(this.element);
 
     this.initComponents();
     await this.renderComponents();
@@ -22,20 +50,37 @@ export default class Page {
   }
 
   initComponents() {
-    const productId = '101-planset-lenovo-yt3-x90l-64-gb-3g-lte-cernyj';
+    const url = new URL('api/rest/products', process.env.BACKEND_URL);
+    url.searchParams.set('_embed', 'subcategory.category');
 
-    this.components.productFrom = new ProductForm(productId);
+    const sortableTable = new SortableTable(productsTableHeader, { url });
+    const doubleSlider = new DoubleSlider({min: 0, max: 4000})
+
+    this.components = { sortableTable, doubleSlider }
   }
 
-  async renderComponents() {
-    const element = await this.components.productFrom.render();
+  renderComponents () {
+    Object.keys(this.components).forEach(component => {
+      const root = this.subElements[component];
+      const { element } = this.components[component];
 
-    this.element.append(element);
+      root.append(element);
+    });
   }
 
   destroy() {
     for (const component of Object.values(this.components)) {
       component.destroy();
     }
+  }
+
+  getSubElements ($element) {
+    const elements = $element.querySelectorAll('[data-element]');
+
+    return [...elements].reduce((accum, subElement) => {
+      accum[subElement.dataset.element] = subElement;
+
+      return accum;
+    }, {});
   }
 }
