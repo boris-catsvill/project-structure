189e5b0def78d29547cb40fd6930dfe1fd4a75e8
Alexander Kryuk <akryuk@beetsoft.com>
2021-07-23 17:39:12 +0600

    - Sidebar collapse handling

--- src/index.js
+++ src/index.js
@@ -15,3 +15,27 @@ router
   .addRoute(/^404\/?$/, 'error404')
   .setNotFoundPagePath('error404')
   .listen();
+
+
+const highlightMenuItem = (event) => {
+  const page = event.detail.page.element.dataset.role;
+  const sidebarNav = document.querySelector('.sidebar__nav');
+  const items = sidebarNav.querySelectorAll('li');
+  [...items].forEach(item => {
+    item.classList.remove('active');
+    const itemPage = item.firstElementChild.dataset.page;
+    if (itemPage === page) {
+      item.classList.add('active');
+    }
+  });
+};
+
+const toggle = document.querySelector('.sidebar__toggler');
+
+toggle.addEventListener('pointerdown', () => document.body.classList.toggle('is-collapsed-sidebar'));
+document.addEventListener('route', highlightMenuItem);
+
+
+
+
+
