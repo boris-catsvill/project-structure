6f2ef5988bae146f64c333753b830966fc1b9da0
Ivanin Alexandr <ivanin.alexandr1994@yandex.ru>
2022-03-27 21:59:07 +0300

    complete added page sales

--- src/pages/products/list/products-headers.js
+++ src/pages/products/list/products-headers.js
@@ -6,7 +6,7 @@ export const headers = [
     template: data => {
       return `
           <div class="sortable-table__cell">
-            <img class="sortable-table-image" alt="Image" src="${data[0].url}">
+            <img class="sortable-table-image" alt="Image" src="${data?.[0]?.url}">
           </div>
         `;
     },
