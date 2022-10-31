90ea6dd567f0fd1708c733c551d6905199b14dc4
exekuta <exekuta@gmail.com>
2022-10-31 16:28:58 +0300

    feat: change code

--- src/components/categories/index.js
+++ src/components/categories/index.js
@@ -1,8 +1,16 @@
+import SortableList from '../../components/sortable-list';
+
 export default class Categories {
   element;
   subElements = {};
 
-  constructor() {
+  constructor({ id, title, count, weight, subcategories }) { 
+    this.id = id;
+    this.title = title;
+    this.count = count;
+    this.weight = weight;
+    this.subcategories = subcategories;
+
     this.render();
   }
 
@@ -10,45 +18,56 @@ export default class Categories {
 
   removeListeners() {}
 
-  // async renderComponents() {
-  //   const { categoriesComponent } = this.subElements;
+  async renderComponents() {
+    const { subcategories } = this.subElements;
+    // const subcategoryItems = this.subcategories.map((item) => )
 
-  //   this.categoriesComponentElement = new 
+    this.sortableListElement = new SortableList({ items: subcategoryItems });
 
-  //   categoriesComponent.append
-  // }
+    subcategories.append(this.sortableListElement.element);
+  }
+
+  renderSubcategories() {
+
+  }
+
+  // <ul class="sortable-list">
+  //   <li class="categories__sortable-list-item sortable-list__item" data-grab-handle=""
+  //       data-id="tovary-dlya-doma">
+  //     <strong>Товары для дома</strong>
+  //     <span><b>11</b> products</span>
+  //   </li>
+  //   <li class="categories__sortable-list-item sortable-list__item" data-grab-handle=""
+  //       data-id="krasota-i-zdorove">
+  //     <strong>Красота и здоровье</strong>
+  //     <span><b>11</b> products</span>
+  //   </li>
+  //   <li class="categories__sortable-list-item sortable-list__item" data-grab-handle=""
+  //       data-id="tovary-dlya-kuxni">
+  //     <strong>Товары для кухни</strong>
+  //     <span><b>13</b> products</span>
+  //   </li>
+  // </ul>
 
   get template() {
     return `
       <div data-element="categoriesContainer">
-        <div class="category category_open" data-id="bytovaya-texnika">
+        <div class="category category_open" data-id="${this.id}">
           <header class="category__header">
-            Бытовая техника
+            ${this.title}
           </header>
           <div class="category__body">
-            <div class="subcategory-list">
-              <ul class="sortable-list">
-                <li class="categories__sortable-list-item sortable-list__item" data-grab-handle=""
-                    data-id="tovary-dlya-doma">
-                  <strong>Товары для дома</strong>
-                  <span><b>11</b> products</span>
-                </li>
-                <li class="categories__sortable-list-item sortable-list__item" data-grab-handle=""
-                    data-id="krasota-i-zdorove">
-                  <strong>Красота и здоровье</strong>
-                  <span><b>11</b> products</span>
-                </li>
-                <li class="categories__sortable-list-item sortable-list__item" data-grab-handle=""
-                    data-id="tovary-dlya-kuxni">
-                  <strong>Товары для кухни</strong>
-                  <span><b>13</b> products</span>
-                </li>
-              </ul>
+            <div class="subcategory-list" data-element="subcategories">
+              
             </div>
           </div>
         </div>
       </div>
     `;
+  }
+
+  getSubcategoriesTemplate() {
+
   }
 
   async render() {
--- src/pages/categories/index.js
+++ src/pages/categories/index.js
@@ -3,30 +3,60 @@ import Categories from '../../components/categories';
 export default class CategoriesPage {
   element;
   subElements = {};
+  categoriesData = [];
 
   // constructor() {
   //   this.render();
   // }
 
-  initListeners() {}
+  // initListeners() {}
 
-  removeListeners() {}
+  // removeListeners() {}
 
-  renderComponents() {
+  // this.categoriesData.map((item) => new Categories(item).forEach((item) => {
+  //   categoriesComponent.append(item.element);
+  // }));
+
+  async getCategoriesData() {
+    const url = new URL('api/rest/categories', process.env.BACKEND_URL);
+    url.searchParams.set('_sort', 'weight');
+    url.searchParams.set('_refs', 'subcategory');
+
+    const data = this.fetchData(url);
+
+    return data;
+  }
+
+  async renderComponents() {
     const { categoriesComponent } = this.subElements;
+    this.categoriesData = await this.getCategoriesData();
+    console.log(this.categoriesData);
 
-    this.categoriesComponentElement = new Categories();
+    this.categoriesData.map((item) => new Categories(item)).forEach((item) => {
+      categoriesComponent.append(item.element);
+    });
 
-    categoriesComponent.append(this.categoriesComponentElement.element);
+    
+    // this.categoriesData
+    //   .map(item => new Categories(item))
+    //   .forEach(item => {
+    //     this.subElements.categoriesContainer.append(item.element);
+    // });
+
+
+    // this.categoriesComponentElement = new Categories();
+
+    // categoriesComponent.append(this.categoriesComponentElement.element);
   }
 
   get template() {
     return `
     <div class="categories">
       <div class="content__top-panel">
-        <h1 class="page-title">Категории товаров</h1>
+        <h1 class="page-title">Product categories</h1>
       </div>
-      
+      <p>Подкатегории можно перетаскивать, меняя их порядок внутри своей категории.</p>
+
       <div data-element="categoriesComponent">
         <!-- categories component -->
       </div>
@@ -42,9 +72,19 @@ export default class CategoriesPage {
     this.subElements = this.getSubElements();
 
     this.renderComponents();
-    this.initListeners();
+    // this.initListeners();
 
     return this.element;
+  }
+
+  async fetchData(url) {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   getSubElements() {
