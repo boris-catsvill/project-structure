7c5e906aaaffd35545ed0bede056d36e80f0030f
Artem <la.pavlov@mail.ru>
2022-11-15 18:43:49 +0400

    first.1 add

--- src/pages/products/list/index.js
+++ src/pages/products/list/index.js
@@ -1,41 +1,74 @@
-import ProductForm from "../../../components/product-form";
+import DoubleSlider from '../../../components/double-slider';
 
 export default class Page {
-  element;
-  subElements = {};
-  components = {};
 
-  async render() {
-    const element = document.createElement('div');
+  constructor() {
+    this.controller = new AbortController();
+    this.render();
+  }
 
-    element.innerHTML = `
-      <div>
-        <h1>List page</h1>
-      </div>`;
+  render() {
+    const div = document.createElement('div');
+    div.innerHTML = this.getTemplate();
 
-    this.element = element.firstElementChild;
+    this.element = div.firstElementChild;
 
     this.initComponents();
-    await this.renderComponents();
 
     return this.element;
   }
 
   initComponents() {
-    const productId = '101-planset-lenovo-yt3-x90l-64-gb-3g-lte-cernyj';
-
-    this.components.productFrom = new ProductForm(productId);
+    this.doubleSlider = new DoubleSlider({
+      min: 100,
+      max: 200,
+      formatValue: value => '$' + value,
+      selected: {
+        from: 100,
+        to: 200
+      }
+    });
+    this.element.querySelector('[data-elem="sliderContainer"]').append(this.doubleSlider.element)
   }
 
-  async renderComponents() {
-    const element = await this.components.productFrom.render();
+  getTemplate() {
+    return `<div class="products-list">
+      <div class="content__top-panel">
+        <h1 class="page-title">Товары</h1>
+        <a href="/products/add" class="button-primary">Добавить товар</a>
+      </div>
+      <div class="content-box content-box_small">
+        <form class="form-inline">
+          <div class="form-group">
+            <label class="form-label">Сортировать по:</label>
+            <input type="text" data-elem="filterName" class="form-control" placeholder="Название товара">
+          </div>
+          <div class="form-group" data-elem="sliderContainer">
+            <label class="form-label">Цена:</label>
+          </div>
+          <div class="form-group">
+            <label class="form-label">Статус:</label>
+            <select class="form-control" data-elem="filterStatus">
+              <option value="" selected="">Любой</option>
+              <option value="1">Активный</option>
+              <option value="0">Неактивный</option>
+            </select>
+          </div>
+        </form>`
+  }
 
-    this.element.append(element);
+  remove() {
+    if (this.element) {
+      this.element.remove()
+    }
   }
 
   destroy() {
-    for (const component of Object.values(this.components)) {
-      component.destroy();
-    }
+    this.remove();
+    this.element = null;
+    this.subElements = {};
+    this.controller.abort();
+    this.components = {};
+
   }
 }
