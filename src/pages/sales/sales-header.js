7105dbe23d4386a10b43e0390f9f5163b97ef848
Egor Tykmanov <egor.tykmanov@glowbyteconsulting.com>
2022-12-06 01:13:32 +0300

    added sales-header

---
+++ src/pages/sales/sales-header.js
@@ -0,0 +1,29 @@
+const header = [
+    {
+        id: 'id',
+        title: 'ID',
+        sortable: true,
+    },
+    {
+        id: 'user',
+        title: 'Client',
+        sortable: true,
+    },
+    {
+        id: 'createdAt',
+        title: 'Date',
+        sortable: true,
+    },
+    {
+        id: 'totalCost',
+        title: 'Price',
+        sortable: true,
+    },
+    {
+        id: 'delivery',
+        title: 'Status',
+        sortable: true,
+    },
+];
+
+export default header;
