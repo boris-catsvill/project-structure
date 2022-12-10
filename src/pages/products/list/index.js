a72d4a167fcd79432ca3a24d20932fff722cfeff
Egor Tykmanov <egor.tykmanov@glowbyteconsulting.com>
2022-12-10 22:55:43 +0300

    implemented products page without listener filters

--- src/pages/products/list/index.js
+++ src/pages/products/list/index.js
@@ -1,4 +1,6 @@
-import ProductForm from "../../../components/product-form";
+import SortableTable from '../../../components/sortable-table/index.js';
+import Doubleslider from '../../../components/double-slider/index.js';
+import header from './products-header.js';
 
 export default class Page {
   element;
@@ -7,30 +9,89 @@ export default class Page {
 
   async render() {
     const element = document.createElement('div');
-
-    element.innerHTML = `
-      <div>
-        <h1>List page</h1>
-      </div>`;
-
+    element.innerHTML = this.getTemplate();
     this.element = element.firstElementChild;
+
+    this.subElements = this.getSubElements();
 
     this.initComponents();
     await this.renderComponents();
 
+    this.initEventListener();
+
     return this.element;
   }
 
-  initComponents() {
-    const productId = '101-planset-lenovo-yt3-x90l-64-gb-3g-lte-cernyj';
+  getSubElements() {
+    const result = {};
+    const elements = this.element.querySelectorAll("[data-element]");
 
-    this.components.productFrom = new ProductForm(productId);
+    for (const subElement of elements) {
+      const name = subElement.dataset.element;
+      result[name] = subElement;
+    }
+
+    return result;
+  }
+
+  getTemplate() {
+    return `
+          <div class="products-list">
+            <div class="content__top-panel">
+              <h2 class="page-title">Products</h2>
+              <a href="/products/add" class="button-primary">Add product</a>
+            </div>
+
+            <div class="content-box content-box_small">
+              <form class="form-inline">
+                <div class="form-group">
+                  <label class="form-label">Sort by:</label>
+                  <input type="text" data-elem="filterName" class="form-control" placeholder="Name product">
+                </div>
+                <div class="form-group" data-element="sliderContainer">
+                  <label class="form-label">Price:</label>
+                  <!-- double-slider component -->
+                </div>
+                <div class="form-group">
+                  <label class="form-label">Status:</label>
+                  <select class="form-control" data-elem="filterStatus">
+                    <option value="" selected="">Any</option>
+                    <option value="1">Active</option>
+                    <option value="0">Inactive</option>
+                  </select>
+                </div>
+              </form>
+            </div>
+
+            <div data-elem="productsContainer" class="products-list__container">
+              <div data-element="sortableTable" class="sortable-table">
+              <!-- sortable-table component -->
+              </div>
+            </div>
+          </div>
+        `;
+  }
+
+  initComponents() {
+    this.components.doubleSlider = new Doubleslider({
+      min: 0,
+      max: 4000,
+      formatValue: value => '$' + value,
+    });
+
+    this.products = {
+      url: `api/rest/products`,
+    };
+    this.components.sortableTable = new SortableTable(header, this.products);
   }
 
   async renderComponents() {
-    const element = await this.components.productFrom.render();
+    this.subElements.sliderContainer.append(this.components.doubleSlider.element);
+    this.subElements.sortableTable.append(this.components.sortableTable.element);
+  }
 
-    this.element.append(element);
+  initEventListener() {
+    //this.element.querySelector('.product-form').addEventListener('product-updated', this.notificationProductUpdated);
   }
 
   destroy() {
