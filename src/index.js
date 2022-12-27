e241e96994b69803b7684dd3ddee6e11a64bbb79
TheAndrey <me@theandrey.name>
2022-12-27 21:01:06 +0200

    Реализовано скрытие боковой панели

--- src/index.js
+++ src/index.js
@@ -15,3 +15,18 @@ router
   .addRoute(/^404\/?$/, 'error404')
   .setNotFoundPagePath('error404')
   .listen();
+
+document.addEventListener('DOMContentLoaded', () => {
+  const sidebarHidden = JSON.parse(localStorage.getItem('sidebar_hidden')) ?? false;
+  if (sidebarHidden) {
+    document.body.classList.add('is-collapsed-sidebar');
+  }
+
+  document.querySelector('.sidebar__toggler').addEventListener('click', event => {
+    event.preventDefault();
+    const isHidden = document.body.classList.toggle('is-collapsed-sidebar');
+
+    localStorage.setItem('sidebar_hidden', JSON.stringify(isHidden));
+  });
+
+}, { once: true });
